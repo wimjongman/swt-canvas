9064c596c027326fb549323cce97ee4a38f0d52d
Wim Jongman <wim.jongman@remainsoftware.com>
2021-02-17 14:51:53 +0100

    updated flowfield example

--- swt.canvas.samples/src/swt/canvas/samples/flowfield/FlowField.java
+++ swt.canvas.samples/src/swt/canvas/samples/flowfield/FlowField.java
@@ -10,7 +10,6 @@ import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Vector;
 
 import swt.canvas.core.Engine;
-import swt.canvas.core.Layout;
 
 public class FlowField extends Engine {
 
@@ -20,7 +19,7 @@ public class FlowField extends Engine {
 
 	private HashMap<Point, Double> fFlowField = new LinkedHashMap<>();
 	private OpenSimplex2S fNoise = new OpenSimplex2S(System.currentTimeMillis());
-	int fNumberOfParticles = 200;
+	int fNumberOfParticles = 20;
 
 	private List<Vector> fParticles = new ArrayList<>();
 	Vector fPreviousParticle = null;
@@ -32,12 +31,10 @@ public class FlowField extends Engine {
 
 	@Override
 	public void draw() {
-		popCanvas();
 		drawString(getFPS() + "", 0, 0);
 		fParticles.forEach(particle -> drawParticle(particle));
-		// drawFlowField();
-		// updateFlowField(1);
-		pushCanvas();
+		drawFlowField();
+		updateFlowField(1);
 	}
 
 	@Override
@@ -45,12 +42,26 @@ public class FlowField extends Engine {
 		fParticles.add(new Vector(x, y));
 	}
 
+	private void updateFlowField(int step) {
+		thirdD += .01;
+		int height = getHeight();
+		int width = getWidth();
+		int start = new Random().nextInt(step);
+		for (int iy = start; iy < height / fResolution; iy += step) {
+			for (int ix = start; ix < width / fResolution; ix += step) {
+				double noise = fNoise.noise3_Classic((ix + .1) / 100, (iy + .1) / 100, thirdD);
+				Point key = new Point(ix, iy);
+				fFlowField.remove(key);
+				fFlowField.put(key, noise);
+			}
+		}
+	}
+
 	private void drawFlowField() {
 		fFlowField.forEach((point, vector) -> {
 			double angle = vector * TWO_PI;
 			int y = (int) (fResolution / 2 * Math.cos(angle));
 			int x = (int) (fResolution / 2 * Math.sin(angle));
-			Vector v = new Vector(x, y);
 			drawLine(point.x * fResolution + (fResolution / 2), point.y * fResolution + (fResolution / 2),
 					point.x * fResolution + (fResolution / 2) + x, point.y * fResolution + (fResolution / 2) + y);
 		});
@@ -58,12 +69,7 @@ public class FlowField extends Engine {
 
 	private void drawParticle(Vector particle) {
 
-//		drawCircle((int) particle.x, (int) particle.y, 10);
-		drawPoint((int) particle.x, (int) particle.y);
-
-//		if (fPreviousParticle != null) {
-//			drawLine((int) particle.x, (int) particle.y, (int) fPreviousParticle.x, (int) fPreviousParticle.y);
-//		}
+		drawCircle((int) particle.x, (int) particle.y, 5);
 
 		Point p = new Point((int) particle.x / fResolution, (int) particle.y / fResolution);
 		double angle = fFlowField.get(p) * TWO_PI;
@@ -83,6 +89,7 @@ public class FlowField extends Engine {
 
 	@Override
 	public void resized(int width, int height) {
+		fParticles.clear();
 		for (int ix = 0; ix < fNumberOfParticles; ix++) {
 			fParticles
 					.add(new Vector(new Random().nextInt(200) + fResolution, new Random().nextInt(200) + fResolution));
@@ -92,34 +99,13 @@ public class FlowField extends Engine {
 
 	@Override
 	public void init() {
-		setAlpha(200);
+		setHeight(400);
+		setWidth(400);
 		setFPS(50);
 	}
 
 	private boolean isOffScreen(Vector particle) {
 		return particle.x < fResolution || particle.x > (getWidth() - fResolution) || particle.y < fResolution
 				|| particle.y > getHeight() - fResolution;
-	}
-
-	private void updateFlowField(int step) {
-		thirdD += .01;
-		int height = getHeight();
-		int width = getWidth();
-		int index = 0;
-		for (int iy = 0; iy < height / fResolution; iy += step) {
-			for (int ix = 0; ix < width / fResolution; ix += step) {
-				double noise = fNoise.noise3_Classic((ix + .01) / 100, (iy + .01) / 100, thirdD);
-				Point key = new Point(ix, iy);
-				fFlowField.remove(key);
-				fFlowField.put(key, noise);
-			}
-		}
-	}
-
-	@Override
-	public Layout getLayout() {
-		return super.getLayout() //
-				.createCell("left", Layout.RIGHT, Layout.CANVAS, 30) //
-				.createCell("bottom", Layout.RIGHT, Layout.CANVAS, 30);
 	}
 }
